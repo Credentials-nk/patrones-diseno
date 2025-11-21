7d9fd6715b96d4ff30bd695ba21d9daad53c16b5
nikoDev <nikolas090189@gmail.com>
2025-11-20 21:50:15 -0300

    abstract factory

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -17,3 +17,86 @@
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+
+// Inicio del patron
+import {COLORS} from '../helpers/colors.ts'
+
+interface Hamburger {
+    prepare(): void;
+}
+
+interface Drink {
+    pour(): void;
+}
+
+
+
+
+class ChickenHamburger implements Hamburger{
+    prepare(): void {
+        console.log('Preparando hamburguesa de %cpollo', COLORS.green)
+    }
+   
+}
+
+class BeefHamburger implements Hamburger{
+    prepare(): void {
+        console.log('Preparando hamburguesa de %ccarne', COLORS.pink)
+    }
+   
+}
+
+class Water implements Drink {
+    pour(): void {
+        console.log('Sirviendo un vaso de %cagua', COLORS.blue)
+    }
+}
+
+class Bear implements Drink {
+    pour(): void {
+        console.log('Sirviendo un vaso de %ccerveza', COLORS.yellow)
+    }
+}
+
+interface RestaurantFactory {
+    createHamburger(): Hamburger
+    createDrink(): Drink
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+
+    createHamburger(): Hamburger {
+        return new BeefHamburger()
+    }
+    createDrink(): Drink {
+        return new Bear()
+    }
+
+}
+
+class HealtyRestaurantFactory implements RestaurantFactory {
+
+    createHamburger(): Hamburger {
+        return new ChickenHamburger()
+    }
+    createDrink(): Drink {
+        return new Water()
+    }
+
+}
+
+
+function main(factory: RestaurantFactory) {
+    const hamburger = factory.createHamburger();
+    const drink = factory.createDrink()
+
+    hamburger.prepare();
+    drink.pour()
+
+}
+
+
+
+
+main(new FastFoodRestaurantFactory)
--- 01-creacionales/03.2-abstract-factory.ts
+++ 01-creacionales/03.2-abstract-factory.ts
@@ -28,6 +28,9 @@
 	  •	Ejecuten el código para asegurarse de que cada fábrica produce el tipo correcto de vehículo y motor.
 
  */
+import {COLORS} from '../helpers/colors.ts'
+
+
 // 1. Interfaces de Vehicle y Engine
 interface Vehicle {
   assemble(): void;
@@ -39,24 +42,37 @@ interface Engine {
 
 // 2. Clases Concretas de Productos
 
-class ElectricCar {
+class ElectricCar implements Vehicle {
   // Implementación del método assemble
   // 'Ensamblando un auto eléctrico'
+  assemble(): void {
+    console.log("Ensamblando %cun auto eléctrico", COLORS.green);
+  }
+
 }
 
-class GasCar {
+class GasCar implements Vehicle {
   // Implementación del método assemble
   // 'Ensamblando un auto de combustión'
+  assemble(): void {
+    console.log('Ensamblando un %cauto de combustión', COLORS.blue);
+  }
 }
 
-class ElectricEngine {
+class ElectricEngine implements Engine {
   // Implementación del método start
   // 'Arrancando motor eléctrico'
+  start(): void {
+    console.log('Arrancando %cmotor eléctrico', COLORS.brown)
+  }
 }
 
-class GasEngine {
+class GasEngine implements Engine{
   // Implementación del método start
   // 'Arrancando motor de combustión'
+  start(): void {
+    console.log('Arrancando %cmotor de combustión', COLORS.cyan)
+  }
 }
 
 // 3. Interfaz de la Fábrica Abstracta
@@ -70,10 +86,22 @@ interface VehicleFactory {
 
 class ElectricVehicleFactory implements VehicleFactory {
   // Implementación de los métodos createVehicle y createEngine
+  createVehicle(): Vehicle {
+    return new ElectricCar()
+  }
+  createEngine(): Engine {
+    return new ElectricEngine()
+  }
 }
 
 class GasVehicleFactory implements VehicleFactory {
   // Implementación de los métodos createVehicle y createEngine
+  createVehicle(): Vehicle {
+    return new GasCar()
+  }
+  createEngine(): Engine {
+    return new GasEngine()
+  }
 }
 
 // 5. Código Cliente
@@ -92,3 +120,6 @@ main(new ElectricVehicleFactory());
 
 console.log('\nCreando vehículo de combustión:');
 main(new GasVehicleFactory());
+
+
+
