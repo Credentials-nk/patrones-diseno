9a55e0c46bb246175196e4cb19a8717fd9708eb9
nikoDev <nikolas090189@gmail.com>
2025-11-25 22:09:08 -0300

    Implement copyWith method in CodeEditorState and Player classes for immutability support

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -6,3 +6,114 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+import {COLORS} from '../helpers/colors.ts'
+
+class CodeEditorState {
+    constructor(
+        readonly content: string,
+        readonly cursorPosition: number,
+        readonly unsavedChanges: boolean
+    ) {
+        this.content = content,
+        this.cursorPosition = cursorPosition,
+        this.unsavedChanges = unsavedChanges
+    }
+
+    copyWith({
+        content,
+        cursorPosition,
+        unsavedChanges
+    }:Partial<CodeEditorState>): CodeEditorState {
+        return new CodeEditorState(
+           content ?? this.content,
+           cursorPosition ?? this.cursorPosition,
+           unsavedChanges ?? this.unsavedChanges 
+        )
+    }
+
+
+    displayState(){
+        console.log('\n%cEstado del Editor:', COLORS.green)
+        console.log(`
+            Contenido: ${this.content}
+            Cursor Pos: ${this.cursorPosition}
+            Unsaved Changes: ${this.unsavedChanges}
+        `)
+    }
+}
+
+class CodeEditorHistory {
+    private history: CodeEditorState[] = [];
+    private currentIndex: number = -1;
+
+    save(state: CodeEditorState): void {
+        if (this.currentIndex < this.history.length - 1) {
+            this.history = this.history.splice(0, this.currentIndex + 1)
+        }
+
+        this.history.push(state)
+        this.currentIndex +=1
+    }
+
+    // Deshacer
+    undo(): CodeEditorState | null {
+        if (this.currentIndex > 0) {
+            this.currentIndex--;
+            return this.history[this.currentIndex]
+        }
+
+        return null
+    }
+
+    // Rehacer
+    redo(): CodeEditorState | null{
+        if (this.currentIndex < this.history.length - 1) {
+            this.currentIndex++;
+            return this.history[this.currentIndex]
+        }
+
+        return null;
+    }
+
+}
+
+
+function main() {
+    const history = new CodeEditorHistory();
+    let editorState = new CodeEditorState(
+        "console.log('Hola mundo');",
+        2,
+        false
+    );
+
+    console.log("%cEstado inicial",COLORS.blue)
+    history.save(editorState);
+    editorState.displayState()
+
+    console.log("%cPrimer cambio",COLORS.blue)
+    editorState = editorState.copyWith({
+        content: "console.log('Hola mundo); \nconsole.log('Nueva Linea');",
+        cursorPosition: 3,
+        unsavedChanges: true
+    })
+    history.save(editorState)
+    editorState.displayState()
+
+    console.log("%cMoviendo el cursor",COLORS.blue)
+    editorState = editorState.copyWith({
+        cursorPosition: 5,
+    })
+    history.save(editorState)
+    editorState.displayState()
+
+    console.log("%cCTRL + Z",COLORS.blue)
+    editorState = history.undo()!
+    editorState.displayState()
+
+    console.log("%cCTRL + Shift + Z",COLORS.blue)
+    editorState = history.redo()!
+    editorState.displayState()
+}
+
+
+main()
