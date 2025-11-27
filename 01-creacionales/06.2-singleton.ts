81371700e20ac22cbd3f3c561eaa95bd2085ed40
nikoDev <nikolas090189@gmail.com>
2025-11-27 20:40:03 -0300

    Implement Singleton pattern in DragonBalls and DatabaseConnection classes with connection management

--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -20,17 +20,35 @@ class DatabaseConnection {
   // Método estático para obtener la instancia única
   public static getInstance(): DatabaseConnection {
     // Completar: implementar el patrón Singleton
-    throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection()
+      console.log("%cDatabase created successfully", COLORS.blue)
+    }
+
+    return DatabaseConnection.instance
   }
 
   // Método para conectar a la base de datos
   public connect(): void {
     // Completar: si no está conectado, mostrar mensaje de conexión
+    if (!this.connected) {
+      this.connected = true
+      console.log("\n%cDatabase connected successfully\n", COLORS.green)
+      return
+    }
+    console.log("%cAn active connection already exists.\n", COLORS.yellow)
   }
-
+  
   // Método para desconectar de la base de datos
   public disconnect(): void {
     // Completar: desconectar y mostrar mensaje de desconexión
+    if (this.connected) {
+      this.connected = false
+      console.log("\n%cDatabase disconnected successfully\n", COLORS.green)
+      return
+    }
+    console.log("%cThere is no active connection..", COLORS.yellow)
   }
 }
 
@@ -47,6 +65,7 @@ function main() {
   db1.disconnect(); // Debería cerrar la conexión
 
   db2.connect(); // Ahora debería conectar de nuevo, ya que se cerró la anterior
+
 }
 
 main();
