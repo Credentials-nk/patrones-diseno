5347d989677c5898299afa4c67672f60b1b5efd4
nikoDev <nikolas090189@gmail.com>
2025-11-30 11:52:50 -0300

    Implement Bridge pattern with Character and Ability classes for flexible attack strategies

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,75 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Ability {
+    use(): void
+}
+
+class SwordAttack implements Ability {
+  use(): void {
+    console.log('%cAttacks fiercely with a sword', COLORS.blue)
+  }
+}
+
+class AxeAttack implements Ability {
+  use(): void {
+    console.log('%cAttacks with an axe', COLORS.orange)
+  }
+}
+
+class MagicSpell implements Ability {
+  use(): void {
+    console.log('%cCasts a powerful magic spell', COLORS.violet)
+  }
+}
+
+class FireballSpell implements Ability {
+  use(): void {
+    console.log('%claunches a powerful fireball', COLORS.red)
+  }
+}
+
+abstract class Character {
+    protected ability: Ability;
+
+    constructor(ability: Ability){
+        this.ability = ability;
+    }
+
+    setAbility(ability: Ability): void{
+        this.ability = ability;
+    }
+
+    abstract performAbility():void;
+
+}
+
+class Warrior extends Character{
+  override performAbility(): void {
+    console.log('\n%cThe warrior is ready to fight', COLORS.brown)
+    this.ability.use()
+  }    
+}
+
+class Mage extends Character{
+  override performAbility(): void {
+    console.log('\n%cThe magician prepares his attack', COLORS.green)
+    this.ability.use()
+  }    
+}
+
+
+function main() {
+    const warrior = new Warrior(new AxeAttack)
+
+    warrior.performAbility()
+
+    const mage = new Mage(new FireballSpell)
+
+    mage.performAbility()
+}
+
+main()
