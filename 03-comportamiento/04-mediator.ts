cdcf4cdccc9d27aec8e73d4d0a6382191de43d57
nikoDev <nikolas090189@gmail.com>
2025-12-14 16:29:40 -0300

    Implement mediator pattern in ChatRoom and ControlTower classes

--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -9,3 +9,71 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+
+
+class ChatRoom {
+    private users: User[] = [];
+    public title: string;
+
+    constructor(title: string) {
+        this.title = title;
+
+    }
+
+    addUser(user: User) {
+        this.users.push(user)
+    }
+
+    sendMessage(sender: User, message: string): void {
+        const receivers = this.users.filter(user => user !== sender)
+        for (const user of receivers) {
+            user.receiveMessage(sender, message)
+            // if (user !== sender) {
+            //     // user.receiveMessage(sender, message)
+            // }
+        }
+    }
+}
+
+class User {
+    constructor(private username: string, private chatRoom: ChatRoom) {
+        chatRoom.addUser(this)
+    }
+
+    sendMessage(message: string): void {
+        console.log(`%c${this.username} envia: %c${message}`,
+            COLORS.blue, COLORS.white
+        )
+        this.chatRoom.sendMessage(this, message);
+    }
+
+    receiveMessage(sender: User, message: string): void {
+        console.log(`%c${this.username} recibe de ${sender.username}: %c${message}`,
+            COLORS.blue, COLORS.white
+        )
+    }
+}
+
+
+function main() {
+    
+    const chatRoom = new ChatRoom('Grupo de trabajo')
+
+    const user1 = new User('Nikolas', chatRoom)
+    const user2 = new User('Fernando', chatRoom)
+    const user3 = new User('Maria', chatRoom)
+
+    console.log('\n')
+    user1.sendMessage('Hola a todos!')
+    console.log('\n')
+    user2.sendMessage('Hola Fernando como estas?')
+    console.log('\n')
+    user3.sendMessage('Hola Nikolas y Fernando como estan?')
+    console.log('\n')
+}
+
+
+main()
