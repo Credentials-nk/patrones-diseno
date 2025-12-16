57176248a127a2e9f1da73ef9e976a7fe4c0c37f
nikoDev <nikolas090189@gmail.com>
2025-12-15 21:25:04 -0300

    Implement Template Method pattern for Hot Beverage preparation with Tea and Coffee classes

--- 03-comportamiento/09-template-method.ts
+++ 03-comportamiento/09-template-method.ts
@@ -14,6 +14,8 @@
  * https://refactoring.guru/es/design-patterns/template-method
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * Contexto: Vamos a implementar un sistema que permite preparar
  * diferentes bebidas calientes, como café y té.
@@ -26,3 +28,55 @@
  * ya que define un esqueleto general del algoritmo en una clase base
  * y delega los detalles específicos a las subclases.
  */
+abstract class HotBerverage {
+
+    prepare():void{
+        this.boilWather();
+        this.addMainIngredient();
+        this.pourInCup();
+        this.addCondiments();
+    }
+
+    private boilWather(){
+        console.log('Hirbiendo agua...')
+    }
+
+    private pourInCup(){
+        console.log('Sirviendo en la taza...')
+    }
+
+    protected abstract addMainIngredient():void;
+    protected abstract addCondiments():void;
+}
+
+
+class Te extends HotBerverage {
+    protected override addMainIngredient(): void {
+      console.log('Añadiendo una bolsa de té')
+    }
+    protected override addCondiments(): void {
+      console.log('Añadiendo miel y limon')
+    }
+}
+
+class Coffee extends HotBerverage {
+    protected override addMainIngredient(): void {
+      console.log('Añadiendo cafe molido')
+    }
+    protected override addCondiments(): void {
+      console.log('Añadiendo azucar y leche')
+    }
+}
+
+function main() {
+    console.log('%cPreparando el té', COLORS.green)
+    
+    const te = new Te;
+    te.prepare()
+    
+    console.log('\n%cPreparando el cafe', COLORS.brown)
+    const coffee = new Coffee;
+    coffee.prepare()
+}
+
+main()
--- 03-comportamiento/09.2-template-method.ts
+++ 03-comportamiento/09.2-template-method.ts
@@ -15,17 +15,17 @@
  */
 
 /**
- * !Objetivo: 
- * Implementar el patrón Template Method para simular un sistema de limpieza 
- * de diferentes tipos de habitaciones 
- * (por ejemplo, una habitación de hotel y una sala de conferencias). 
- * 
- * Debes diseñar una clase base que defina el flujo general de limpieza 
- * y subclases que implementen pasos específicos dependiendo del tipo 
+ * !Objetivo:
+ * Implementar el patrón Template Method para simular un sistema de limpieza
+ * de diferentes tipos de habitaciones
+ * (por ejemplo, una habitación de hotel y una sala de conferencias).
+ *
+ * Debes diseñar una clase base que defina el flujo general de limpieza
+ * y subclases que implementen pasos específicos dependiendo del tipo
  * de habitación.
- * 
- * 
- * 
+ *
+ *
+ *
  * ! Descripción del Ejercicio
   El proceso de limpieza general incluye los siguientes pasos:
     1.	Entrar a la habitación: Abrir la puerta y entrar.
@@ -34,11 +34,11 @@
     •	En una habitación de hotel, se hacen las camas.
     •	En una sala de conferencias, se limpian las mesas y se organizan las sillas.
     • En una oficina, se limpian los escritorios y se organizan los documentos.
-    4.	Desinfectar superficies: Desinfectar las áreas principales.
+    4.	Desinfectar superficies: Desinfectar las áreas principales. // ramirito => ddd.  mn b.       j m,m hc   bvbh
     5.	Salir de la habitación: Cerrar la puerta y marcar como terminada
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // Clase Base - RoomCleaning
 abstract class RoomCleaning {
@@ -50,60 +50,79 @@ abstract class RoomCleaning {
     this.disinfectSurfaces();
     this.exitRoom();
 
-    console.log('Limpieza terminada.\n');
+    console.log("Limpieza terminada.\n");
   }
 
   // Pasos comunes
   private enterRoom(): void {
-    console.log('Entrando a la habitación...');
+    console.log("Entrando a la habitación...");
   }
 
   private collectTrash(): void {
-    console.log('Recogiendo la basura...');
+    console.log("Recogiendo la basura...");
   }
 
   private disinfectSurfaces(): void {
-    console.log('Desinfectando superficies...');
+    console.log("Desinfectando superficies...");
   }
 
   private exitRoom(): void {
-    console.log('Saliendo de la habitación y marcándola como limpia.');
+    console.log("Saliendo de la habitación y marcándola como limpia.");
   }
 
   // Método abstracto para limpieza específica
   // TODO: Implementar el método specificCleaning en las subclases
   // protected abstract? ambas?
+  protected abstract specificCleaning(): void;
 }
 
 // Subclase - HotelRoomCleaning
 class HotelRoomCleaning extends RoomCleaning {
   // TODO: Implementar método específico
   // Mensaje: 'Haciendo las camas y reponiendo artículos de baño.'
+  protected override specificCleaning(): void {
+    console.log(
+      "%cHaciendo las camas y reponiendo artículos de baño.",
+      COLORS.blue,
+    );
+  }
 }
 
 // Subclase - ConferenceRoomCleaning
 class ConferenceRoomCleaning extends RoomCleaning {
   // TODO: Implementar método específico
   // Mensaje: 'Limpiando mesas y organizando sillas.'
+  protected override specificCleaning(): void {
+    console.log(
+      "%cLimpiando mesas y organizando sillas.",
+      COLORS.orange,
+    );
+  }
 }
 
 // Subclase - OfficeCleaning
 class OfficeCleaning extends RoomCleaning {
   // TODO: Implementar método específico
   // Mensaje: 'Limpiando escritorios y organizando documentos.'
+    protected override specificCleaning(): void {
+    console.log(
+      "%cLimpiando escritorios y organizando documentos",
+      COLORS.pink,
+    );
+  }
 }
 
 // Código Cliente
 function main(): void {
-  console.log('%cLimpieza de una habitación de hotel:', COLORS.blue);
+  console.log("%cLimpieza de una habitación de hotel:", COLORS.blue);
   const hotelRoom = new HotelRoomCleaning();
   hotelRoom.cleanRoom();
 
-  console.log('%cLimpieza de una sala de conferencias:', COLORS.purple);
+  console.log("%cLimpieza de una sala de conferencias:", COLORS.purple);
   const conferenceRoom = new ConferenceRoomCleaning();
   conferenceRoom.cleanRoom();
 
-  console.log('%cLimpieza de una oficina:', COLORS.orange);
+  console.log("%cLimpieza de una oficina:", COLORS.orange);
   const office = new OfficeCleaning();
   office.cleanRoom();
 }
